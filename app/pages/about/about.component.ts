1d65912db335cd7ce723d4d8e2e7f4bc7650db5c
krutikagsfj <49934320+krutikagsfj@users.noreply.github.com>
2019-04-25 09:46:40 +0530

    Add files via upload

---
+++ app/pages/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { 
+  }
+  title = 'About Page of this Application';
+  ngOnInit() {
+  }
+
+}
---
+++ app/pages/product-brands/product-brands.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { GetDataService } from 'src/app/get-data.service';
+
+
+@Component({
+  selector: 'app-product-brands',
+  templateUrl: './product-brands.component.html',
+  styleUrls: ['./product-brands.component.css']
+})
+export class ProductBrandsComponent implements OnInit {
+
+  constructor(private srvis :GetDataService) {
+  }
+  title='Product Brand';
+  data1;
+  ngOnInit() {
+      this.srvis.getProducts().subscribe(res=>{
+          this.data1=res;
+          console.log(res);
+      })
+ }
+}
